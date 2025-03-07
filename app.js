d26984bcbc9ee062ec0ec97f3a62a6735a78ffa2
Emil Jiménez Ortiz <114709881+emiljimenez86@users.noreply.github.com>
2025-03-07 14:50:21 -0500

    Segundo commit

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ if ('serviceWorker' in navigator) {
         .then(reg => console.log('Service Worker registrado', reg))
         .catch(err => console.log('Error al registrar el Service Worker', err));
 }
+
+let deferredPrompt;
+        const installButton = document.getElementById("installButton");
+
+        window.addEventListener("beforeinstallprompt", (event) => {
+            event.preventDefault();
+            deferredPrompt = event;
+            installButton.style.display = "block";
+        });
+
+        installButton.addEventListener("click", () => {
+            if (deferredPrompt) {
+                deferredPrompt.prompt();
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === "accepted") {
+                        console.log("Usuario instaló la aplicación");
+                    }
+                    deferredPrompt = null;
+                    installButton.style.display = "none";
+                });
+            }
+        });
